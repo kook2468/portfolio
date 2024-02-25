68a17a5a6505480e0c22eb96a67c905a22b2c793
kook2468 <cdg9808@naver.com>
2024-02-25 21:57:48 +0900

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  compiler: {
+    //styledComponents 활성화
+    styledComponents: true,
+  },
 };
 
 export default nextConfig;
